b913f91d42602ff0c5d951038f161ef8547256d4
Aarash <aarash428@gmail.com>
2017-05-01 14:47:27 +0545

    edit

--- app/controllers/sims_controller.rb
+++ app/controllers/sims_controller.rb
@@ -8,9 +8,27 @@ class SimsController < ApplicationController
 		redirect_to root_path
 	end
 
+	
+	def edit
+		@idea = Sim.find(params[: id])
+	end
+
+	def update
+		@sim = Sim.find(params[:id])
+		if @sim.update(sim_params)
+		   redirect_to root_path
+		 else
+		   redirect_to edit_sim_path(params[:id])
+		end
+    end
+
+	def new
+	end
+
 	private
 
 	def sim_params
 		params.require(:sim).permit(:date, :title, :amount)
 	end
+
 end
