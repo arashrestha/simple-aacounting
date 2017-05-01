079dd3f7a5dad13e27b14826a355664aa34d334d
Aarash <aarash428@gmail.com>
2017-05-01 13:54:35 +0545

    create

--- app/controllers/sims_controller.rb
+++ app/controllers/sims_controller.rb
@@ -2,4 +2,15 @@ class SimsController < ApplicationController
 	def index
 		@sims = Sim.all
 	end
+
+	def create
+		@sim = Sim.create(sim_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def sim_params
+		params.require(:sim).permit(:date, :title, :amount)
+	end
 end
